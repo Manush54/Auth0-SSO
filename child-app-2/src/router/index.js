5b05ae0f68ab6dd47a92772de10a5ca77ea26fcd
Anita Kumari <anita.kumari@ad.kpmgsofy.com>
2024-05-21 14:25:58 +0530

    full app deploy

---
+++ child-app-2/src/router/index.js
@@ -0,0 +1,56 @@
+import HomePage from "@/pages/home-page.vue";
+import CallbackPage from "@/pages/callback-page.vue";
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+const NotFoundPage = () => import("@/pages/not-found-page.vue");
+const ProfilePage = () => import("@/pages/profile-page.vue");
+const PublicPage = () => import("@/pages/public-page.vue");
+const ProtectedPage = () => import("@/pages/protected-page.vue");
+const AdminPage = () => import("@/pages/admin-page.vue");
+
+const router = new Router({
+  mode: "history",
+  base: import.meta.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomePage,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: ProfilePage,
+    },
+    {
+      path: "/public",
+      name: "public",
+      component: PublicPage,
+    },
+    {
+      path: "/protected",
+      name: "protected",
+      component: ProtectedPage,
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: AdminPage,
+    },
+    {
+      path: "/callback",
+      name: "callback",
+      component: CallbackPage,
+    },
+    {
+      path: "*",
+      name: "Not Found",
+      component: NotFoundPage,
+    },
+  ],
+});
+
+export default router;
---
+++ root-app/src/components/navigation/desktop/nav-bar-buttons.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav-bar__buttons">
+    {{ $auth0.isAuthenticated }}
+    <template v-if="!$auth0.isAuthenticated">
+      <SignupButton />
+      <LoginButton />
+    </template>
+    <template v-if="$auth0.isAuthenticated">
+      <LogoutButton />
+    </template>
+  </div>
+</template>
+
+<script>
+import LoginButton from "@/components/buttons/login-button.vue";
+import LogoutButton from "@/components/buttons/logout-button.vue";
+import SignupButton from "@/components/buttons/signup-button.vue";
+
+export default {
+  components: { LoginButton, LogoutButton, SignupButton },
+};
+</script>
